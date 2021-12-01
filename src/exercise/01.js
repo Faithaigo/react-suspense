1ad76dd92cd69d1a4ee52805f98a215cbcedeea6
faith <faith@marascientific.org>
2021-12-01 20:46:30 +0300

    Add error boundary

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,15 +2,19 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon} from '../pokemon'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 
 
 let pokemon;
-let pokemonPromise = fetchPokemon('pikachu').then(value=>pokemon = value, err=>console.log(err))
+let error;
+let pokemonPromise = fetchPokemon('pikach').then(value=>pokemon = value, err=>error = err)
 
 function PokemonInfo() {
   if(!pokemon){
     throw pokemonPromise
+  }
+  if(error){
+    throw error
   }
 
   return (
@@ -27,9 +31,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
+      <PokemonErrorBoundary>
         <React.Suspense fallback={<div>Loading....</div>}>
-        <PokemonInfo />
+            <PokemonInfo />
         </React.Suspense>
+      </PokemonErrorBoundary>
       </div>
     </div>
   )
