5a3c0ef9b6446b08e1152293247dc57a70ae7218
faith <faith@marascientific.org>
2021-12-03 12:42:00 +0300

    Cache pokemon resource

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -29,16 +29,16 @@ const SUSPENSE_CONFIG = {
   busyMinDurationMs: 700,
 }
 
-// 🐨 create a pokemonResourceCache object
-
-// 🐨 create a getPokemonResource function which accepts a name checks the cache
-// for an existing resource. If there is none, then it creates a resource
-// and inserts it into the cache. Finally the function should return the
-// resource.
-
-function createPokemonResource(pokemonName) {
-  return createResource(fetchPokemon(pokemonName))
+const pokemonResourceCache = {}
+function getPokemonResource(pokemonName){
+  let resource = pokemonResourceCache[pokemonName];
+  if(!resource){  
+    resource = createResource(fetchPokemon(pokemonName))
+    pokemonResourceCache[pokemonName] = resource;
+  }
+  return resource
 }
+
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
@@ -51,8 +51,7 @@ function App() {
       return
     }
     startTransition(() => {
-      // 🐨 change this to getPokemonResource instead
-      setPokemonResource(createPokemonResource(pokemonName))
+      setPokemonResource(getPokemonResource(pokemonName))
     })
   }, [pokemonName, startTransition])
 
